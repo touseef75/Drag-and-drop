c4826bbe4435643d9d01f9116b116b72be8641aa
touseef75 <imrantouseef776@gmail.com>
2021-12-07 00:19:42 -0800

    set

--- Gram positive and negative bactarie/Assets/main.js
+++ Gram positive and negative bactarie/Assets/main.js
@@ -20,17 +20,14 @@ let arr = [
     {
         video:"./images/step.mp4",
         cont:"#414953",
-        div:"#83898e",
     },
      {
         video:"./images/Gram Staining Procedure Animation Microbiology - Principle, Procedure, Interpretation (online-video-cutter.com).mp4",
         cont:"#25588c",
-        div:"#cde7ed",
     },
     {
         video:"./images/step3 (3).mp4",
         cont:"#1272ba",
-        div:"#b3dde8",
 
     },
 ]
@@ -40,12 +37,10 @@ but.addEventListener("click",function(){
     if(num == 2){
         video.src = arr[num].video;
     cont.style.background = arr[num].cont;
-    document.getElementById("ko").style.background = arr[num].div;
     num = -1;
     }else{
         video.src = arr[num].video;
     cont.style.background = arr[num].cont;
-    document.getElementById("ko").style.background = arr[num].div;
     }
     
 });
